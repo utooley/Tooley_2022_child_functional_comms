3101c249ebca626661323ce044f35bea5ba0d8f3
utooley <utooley@gmail.com>
2020-03-25 16:21:42 -0400

    upsampling to fsaverage6

--- code/yeo_networks/visualization.m
+++ code/yeo_networks/visualization.m
@@ -1,6 +1,6 @@
 %Checking Yeo code outputs
-cluster_dir='/cbica/projects/spatial_topography/data/imageData/yeo_clustering_networks/yeo7_n670_2runsonly_1000tries_2/'
-file_name='yeo7_n670_2runsonly_1000tries_2.mat'
+cluster_dir='/cbica/projects/spatial_topography/data/imageData/yeo_clustering_networks/yeo7_n670_2runsonly_1000tries/'
+file_name='yeo7_n670_2runsonly_1000tries.mat'
 
 clustered = load(fullfile(cluster_dir,file_name));
 
@@ -23,12 +23,43 @@ CBIG_DrawSurfaceMaps(clustered.lh_labels, clustered.rh_labels, 'fsaverage6','inf
 
 CBIG_DrawSurfaceMaps(clustered.lh_labels, clustered.rh_labels, 'fsaverage5','inflated',0, 17, ref.colors)
 
+%read in WSBM data and relabel Schaefer400 annot file with WSBM assignments
+%can use CBIG_read_annotation.m
+%then use CBIG_VisualizeSurfaceAnnotationInFreeview to view.
+
+%% Save developmental communities to Freesurfer annotation
+%CBIG_SaveParcellationToFreesurferAnnotation(fullfile(cluster_dir,file_name), 'lh.yeodev.fsaverage.annot', 'rh.yeodev.fsaverage.annot')
+%this uses the colors in the wrong order.
+
+CBIG_WriteParcellationToAnnotation(clustered.lh_labels,fullfile(cluster_dir,'lh.yeodev.fsaverage6.annot'), ref.colors)
+CBIG_WriteParcellationToAnnotation(clustered.rh_labels,fullfile(cluster_dir,'rh.yeodev.fsaverage6.annot'), ref.colors)
+%this is what I want! With my colors and in fsaverage6 space.
+
+%% Confidence maps
+%Plot Yeo7 confidence maps
+yeo_ref_dir='/cbica/projects/spatial_topography/tools/CBIG/stable_projects/brain_parcellation/Yeo2011_fcMRI_clustering/1000subjects_reference/'
+
+CBIG_DrawSurfaceMaps(ref.lh_s, ref.rh_s, 'fsaverage5','inflated',0, 0.4)
+%make a colormap by hand in the GUI to save with the annotation
+
+%and save them as an annotation
+CBIG_WriteParcellationToAnnotation(ref.lh_s,fullfile(yeo_ref_dir,'lh.silhouette.fsaverage5.annot'), mycmap)
+CBIG_WriteParcellationToAnnotation(ref.rh_s,fullfile(yeo_ref_dir,'rh.silhouette.fsaverage5.annot'), ref.colors)
+CBIG_WriteParcellationToAnnotation(clustered.rh_labels,fullfile(cluster_dir,'rh.yeodev.fsaverage6.annot'), ref.colors)
+
+abs_path_to_lh_annot_file = fullfile(getenv('FREESURFER_HOME'), 'subjects', 'fsaverage5', 'label', 'lh.aparc.annot');
+abs_path_to_rh_annot_file = fullfile(getenv('FREESURFER_HOME'), 'subjects', 'fsaverage5', 'label', 'rh.aparc.annot');
+
+CBIG_DrawSurfaceDataAsAnnotation(ref.lh_s, ref.rh_s, abs_path_to_lh_annot_file, abs_path_to_rh_annot_file, 0, 'fsaverage5', yeo_ref_dir, 'test')
+
+[test colortable]=CBIG_ConvertSingleHemiSurfaceDataToDiscretizedAnnotation(ref.lh_s, 15, 'hsv',0,1)  
+read_annotation('/cbica/projects/spatial_topography/tools/CBIG/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/FreeSurfer5.3/fsaverage5/lh.Schaefer2018_400Parcels_7Networks_order.annot')
+
+
 %Silhouettes/confidence maps-can change the colors?
 load('/cbica/projects/spatial_topography/data/imageData/yeo_clustering_networks/yeo7_n670_2runsonly_1000tries/yeo7_n670_2runsonly_1000tries.mat')
 load('/cbica/projects/spatial_topography/data/imageData/yeo_clustering_networks/yeo7_n131_5tries/silhou_cmap.mat')
 
 CBIG_DrawSurfaceMaps(clustered.lh_s, clustered.rh_s, 'fsaverage6','pial',0, 0.4, color)
 
-%read in WSBM data and relabel Schaefer400 annot file with WSBM assignments
-%can use CBIG_read_annotation.m
-%then use CBIG_VisualizeSurfaceAnnotationInFreeview to view.
+
