447567d5ceb8c768c1822608b5374987e57b5380
utooley <utooley@gmail.com>
2020-03-26 13:17:11 -0400

    relabeling annotation file with wsbm

--- code/analysis/extract_surface_area.sh
+++ code/analysis/extract_surface_area.sh
@@ -1,4 +1,6 @@
 
+### FOR YEO7 AND YEO-DEVELOPMENTAL ###
+
 declare -a array=(lh
 rh)
 
@@ -19,4 +21,18 @@ do
 annot_file=/cbica/projects/spatial_topography/data/imageData/yeo_clustering_networks/yeo7_n670_2runsonly_1000tries/${hemi}.yeodev.fsaverage6.annot
 #get surface area
 mri_segstats --annot fsaverage6 lh $annot_file --i $SUBJECTS_DIR/fsaverage6/surf/${hemi}.white.avg.area.mgh --accumulate --sum freesurfer/${hemi}.surfarea.stats
+done
+
+### FOR WSBM AND YEO7 ###
+#first, relabel the Schaefer fsaverage6 annotation to have WSBM labels with visualization.m, then calculate surface area.
+
+declare -a array=(lh
+rh)
+
+for hemi in "${array[@]}"
+do
+annot_file=/cbica/projects/spatial_topography/data/imageData/wsbm/site16_training_sample/brains/${hemi}.wsbm.consensus.fsaverage6.annot
+
+#get surface area
+mri_segstats --annot fsaverage6 lh $annot_file --i $SUBJECTS_DIR/fsaverage6/surf/${hemi}.white.avg.area.mgh --accumulate --sum ${hemi}.surfarea.stats
 done
