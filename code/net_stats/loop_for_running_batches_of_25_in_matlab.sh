24ef9cef58caa93c36951837a7cd167fb89ea997
utooley <utooley@gmail.com>
2020-04-07 09:54:32 -0400

    fixed typo in running batches of 25

--- code/net_stats/loop_for_running_batches_of_25_in_matlab.sh
+++ code/net_stats/loop_for_running_batches_of_25_in_matlab.sh
@@ -2,13 +2,12 @@
 code_dir=/cbica/projects/spatial_topography/code/net_stats
 subject_list=/cbica/projects/spatial_topography/data/subjLists/release2/site16/n670_subjects_only_filtered_runs_site16_postprocess.txt
 
-x=0 #increment this by 1 each time people stop running.
+x=6 #increment this by 1 each time people stop running. Run in batches of 25
 
 mapfile -t myArray < $subject_list
 begin=$(expr ${x} \* 25)
-end=$(expr 25 + ${x} \* 25)
 
-for i in "${myArray[@]:$begin:$end}";
+for i in "${myArray[@]:${begin}:25}";
 do
 echo $i
 qsub ${code_dir}/run_matlab_net_stats.sh ${i}
